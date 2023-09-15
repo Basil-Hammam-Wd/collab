fe46838222a0341533a18bf819b7a27a4191a37e
Basil-Hammam-Wd <basilaza0897@gmail.com>
2023-09-16 02:18:11 +0700

    collab with designer

---
+++ script/script.js
@@ -0,0 +1,13 @@
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navLists = document.querySelector(".nav");
+
+  burger.addEventListener("click", () => {
+    navLists.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
+};
+
+navSlide();
+
+// mengambil sedikit dari script kemaren portfolio
